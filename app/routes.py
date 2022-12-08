01f3cbecb2226ec096f876b65e651081c29bd720
Andrzej Radzik <andrzej.radzik@onet.pl>
2022-12-08 20:15:46 +0100

    Added user registration and login

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,61 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user
+from werkzeug.urls import url_parse
+
+from app import app, db
+from app.forms import LoginForm, RegisterForm
+from app.models import User
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            flash('Invalid username')
+            return redirect(url_for('login'))
+        if not user.check_password(form.password.data):
+            flash('Invalid password')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        
+        return redirect(next_page)
+    return render_template('login.html', title='Log In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration was succesful!')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('test.html', title='Test')
+    return render_template('main.html', title='Start page')
 
